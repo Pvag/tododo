294c30281769e6374e28eb5d67842f269a0f89d1
umberto82 <ufo1982u@gmail.com>
2025-08-10 12:18:09 +0200

    add header

--- index.php
+++ index.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>To Do Do</title>
-    <script src="./script.js" defer></script>
-    <link rel="stylesheet" href="output.css">
-</head>
-
-<body>
-    <?php
-    echo '<h1 class="bg-red-300 mt-8 text-gray-600 p-5 rounded-2xl">To Do Do</h1>';
-    echo '<h3 class="mt-[20px] text-white">..a project with Slurmenton</h3>';
-    echo '<p>wow</p>';
-    ?>
+<?php
+include("./header.php");
+echo '<h1 class="bg-red-300 mt-8 text-gray-600 p-5 rounded-2xl">To Do Do</h1>';
+echo '<h3 class="mt-[20px] text-white">..a project with Slurmenton</h3>';
+echo '<p>wow</p>';
+?>
 </body>
 
 </html>
