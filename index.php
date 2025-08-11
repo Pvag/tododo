09f2a568221a6624f770f26ab1d9c4b7f99e8202
umberto82 <ufo1982u@gmail.com>
2025-08-11 17:18:55 +0200

    add footer

--- index.php
+++ index.php
@@ -3,7 +3,4 @@ include("./header.php");
 echo '<h1 class="bg-red-300 mt-8 text-gray-600 p-5 rounded-2xl">To Do Do</h1>';
 echo '<h3 class="mt-[20px] text-white">..a project with Slurmenton</h3>';
 echo '<p>wow</p>';
-?>
-</body>
-
-</html>
+include("./footer.php");
